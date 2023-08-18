720d07a8e9b6c8b3f1ae9e5f7c540a29c827956f
Nega García <nega_gl@outlook.com>
2023-08-18 16:24:15 -0500

    Changed options for a dict

--- main.py
+++ main.py
@@ -2,38 +2,36 @@
 import random
 
 print("Welcome to Rock, Paper, Scissors game!!")
-
-options = ["rock", "paper", "scissors"]
 player_score = 0
 bot_score = 0
+
+options_dict = {
+    "rock": "scissors",
+    "paper": "rock",
+    "scissors": "paper",
+}
+
+keys = list(options_dict.keys())
 
 while True:
     # Ask the user choice
     print("--------------------------------")
     user_choice = input("Please choose your weapon (Rock/Paper/Scissors) or type Q to quit game: ").lower()
 
-    if user_choice not in options:
+    if user_choice not in keys:
         if user_choice == "q":
             break
         else:
             print("Please choose a valid option...")
             continue
 
-    bot_choice = options[random.randrange(0, len(options))]
+    bot_choice = keys[random.randrange(0, len(keys))]
     print(f"Bot choice is: {bot_choice}. Your choice is: {user_choice}")
 
     if user_choice == bot_choice:
         print("It's a tie!")
         continue
-    elif user_choice == "rock" and bot_choice == "scissors":
-        print("You win this round!")
-        player_score += 1
-        continue
-    elif user_choice == "paper" and bot_choice == "rock":
-        print("You win this round!")
-        player_score += 1
-        continue
-    elif user_choice == "scissors" and bot_choice == "paper":
+    elif bot_choice == options_dict[user_choice]:
         print("You win this round!")
         player_score += 1
         continue
